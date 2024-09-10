12da62bd144576f6bfa889b59bfbd803b8f1d6be
letnagao <leticia-nagao@hotmail.com>
2024-09-10 13:16:34 -0300

    Lunch time hihi

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -7,11 +7,84 @@ import user_3 from "../../assets/user-3.png";
 import user_4 from "../../assets/user-4.png";
 
 const Testimonials = () => {
+  const slideForward = () => {};
+
+  const slideBackward = () => {};
   return (
     <div className="testimonials">
-      <img src={next_icon} alt="" className="next-btn" />
-      <img src={back_icon} alt="" className="back-btn" />
-      <div className="slider"></div>
+      <img src={next_icon} alt="" className="next-btn" onClick={slideForward} />
+      <img
+        src={back_icon}
+        alt=""
+        className="back-btn"
+        onClick={slideBackward}
+      />
+      <div className="slider">
+        <ul>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_1} alt="" />
+                <div>
+                  <h3>Name etc</h3>
+                  <span>Sao Paulo, BRASIL</span>
+                </div>
+              </div>
+              <p>
+                Walk the layout, routines in the night Some doors have STAY OUT
+                spray-painted in white While all the worlds asleep, I walk
+                around instead
+              </p>
+            </div>
+          </li>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_2} alt="" />
+                <div>
+                  <h3>Name etc</h3>
+                  <span>Sao Paulo, BRASIL</span>
+                </div>
+              </div>
+              <p>
+                Through the memories, down the halls of my head Walk the layout,
+                routines in the night Some doors have STAY OUT spray-painted in
+                white
+              </p>
+            </div>
+          </li>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_3} alt="" />
+                <div>
+                  <h3>Name etc</h3>
+                  <span>Sao Paulo, BRASIL</span>
+                </div>
+              </div>
+              <p>
+                Im testin the limits of what a mind can do Im keepin my
+                eyelids up no matter what
+              </p>
+            </div>
+          </li>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_4} alt="" />
+                <div>
+                  <h3>Name etc</h3>
+                  <span>Sao Paulo, BRASIL</span>
+                </div>
+              </div>
+              <p>
+                So beautiful, the space between A painful reminder and a
+                terrible dream
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
